27ea88c22882df96f2ad86bb6506d492dc3f0b68
Juliana Lucca <lucca.editora@gmail.com>
2023-04-12 15:27:06 -0300

    create form validation and button event

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -4,7 +4,8 @@ const ListaSuspensa = (props) => {
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select>
+            <select required={props.obrigatorio} defaultValue="">
+                <option value="" disabled>Selecione...</option>
                 {props.itens.map( (item) => <option key={item}>{item}</option>)}
             </select>
         </div>
