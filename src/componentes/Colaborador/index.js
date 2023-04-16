3b499f7416d1d9fd8122cbf98c996fb49849c185
Juliana Lucca <lucca.editora@gmail.com>
2023-04-15 21:19:26 -0300

    create card component

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src="https://github.com/julucca.png" alt="Foto de perfil da Juliana Lucca" />
+            </div>
+            <div className='rodape'>
+                <h4>Juliana Lucca</h4>
+                <h5>Desenvolvedora Front-End</h5>
+            </div>
+        </div>
+    );
+};
+
+export default Colaborador;
