cff5fb2bf3b33ad23bfbec8c7b489d8c2e9e5a3c
Juliana Lucca <lucca.editora@gmail.com>
2023-04-16 14:58:49 -0300

    fix bug in team field

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -4,8 +4,8 @@ const ListaSuspensa = (props) => {
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.value} defaultValue="">
-                <option value="" disabled>Selecione...</option>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.valor}>
+                <option value=""></option>
                 {props.itens.map( (item) => <option key={item}>{item}</option>)}
             </select>
         </div>
